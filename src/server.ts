e05bbea7f0c16d31b6265d60c8a31641c93c1da2
Bueno <zymbabueno@gmail.com>
2020-04-13 23:21:52 -0300

    Configurado editorConfig e ESLint

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,8 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-  res.json({ message: 'hello' })
-})
+  res.json({ message: 'hello' });
+});
 
 app.listen(3333, () => {
   console.log('server started on port 3333');
