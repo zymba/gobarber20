e75ab69f51ba7f29ac5ef542a3fadb3db98fb1b4
Bueno <zymbabueno@gmail.com>
2020-04-15 19:58:10 -0300

    Separei o arquivo de rotas e criei a rota post appointments

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,8 @@
 import { Router } from 'express';
+import appointmentsRouter from './appointments.routes';
 
 const routes = Router();
 
-routes.get('/', (req, res) => {
-  res.json({ message: 'hello' });
-});
+routes.use('/appointments', appointmentsRouter);
 
 export default routes;
