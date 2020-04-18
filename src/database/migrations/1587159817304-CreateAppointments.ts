34411e37c6b2186e0552229fb4a772dbd4e9bb49
Bueno <zymbabueno@gmail.com>
2020-04-18 14:36:11 -0300

    Alterações no repository e na service para gravar agendamentos no banco usando TypeORM

--- src/database/migrations/1587159817304-CreateAppointments.ts
+++ src/database/migrations/1587159817304-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1587159817304
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
