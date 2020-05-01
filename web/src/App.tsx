e5bebcc7300791b659514cb57952a321ef5ab270
Bueno <zymbabueno@gmail.com>
2020-05-01 13:01:16 -0300

    Página de login

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
+import SignIn from './pages/SignIn';
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
   <>
-    <div>Hello from App</div>
+    <SignIn />
     <GlobalStyle />
   </>
 );
