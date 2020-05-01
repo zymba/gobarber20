d61430e82c6dd7601f8d3a1a4c7f183cf537345e
Bueno <zymbabueno@gmail.com>
2020-05-01 19:31:40 -0300

    Usabilidade nos inputs do formulário

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -36,6 +36,12 @@ module.exports = {
       "react-hooks/exhaustive-deps": "warn",
       "react/jsx-filename-extension": [1, { "extensions": [".tsx"] }],
       "import/prefer-default-export": "off",
+      "@typescript-eslint/explicit-function-return-type": [
+        "error",
+        {
+          "allowExpressions": true
+        }
+      ],
       "import/extensions": [
         "error",
         "ignorePackages",
